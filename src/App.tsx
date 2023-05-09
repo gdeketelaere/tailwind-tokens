21a8f28d92828fbe8472a6b187350bc38e9e5dd6
Gilles Deketelaere <gdeketelaere@gmail.com>
2023-05-09 15:17:48 +0200

    🎨 Add storybook to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/Buttons/buttons";
 import "./style/App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold mb-8">Hello tokens</h1>
+      <Button />
     </div>
   );
 }
